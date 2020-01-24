4882459a5469989bb125d8ea301b025dc7a0d264
gamesguru <mathmuncher11@gmail.com>
2020-01-24 16:15:39 -0500

    go back to simple approach, usda only for now

--- data/setup.sh
+++ data/setup.sh
@@ -15,58 +15,41 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+#
 
 # cd to script's directory
 cd "$(dirname "$0")"
 
 # Create tmp dir
-mkdir tmp
+mkdir -p tmp
 cd tmp
 
-
-
+#
 # Download and unzip
 curl -O "https://www.ars.usda.gov/ARSUserFiles/80400525/Data/SR-Legacy/SR-Leg_DB.zip"
-curl -O "https://www.ars.usda.gov/ARSUserFiles/80400525/Data/BFPDB/BFPD_db_07132018.zip"
-curl -O "https://www.canada.ca/content/dam/hc-sc/migration/hc-sc/fn-an/alt_formats/zip/nutrition/fiche-nutri-data/cnf-fcen-csv.zip"
 unzip SR-Leg_DB.zip SR_Legacy.accdb
-unzip BFPD_db_07132018.zip BFPD_07132018.accdb
-unzip cnf-fcen-csv.zip -d cnf/
 
-mkdir ofdb
-curl -o ofdb/en.openfoodfacts.org.products.csv "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv"
-
-
-
+#
 # Run access2csv
 git clone git@github.com:AccelerationNet/access2csv.git
 
 cd access2csv
-mvn clean install
+mvn clean install -Dmaven.test.skip=true
 ./access2csv --input ../SR_Legacy.accdb --output ../usda --with-header
-./access2csv --input ../BFPD_07132018.accdb --output ../bfdb --with-header
 cd ..
 
+#
+# Move to permanent home
+mkdir -p ../csv
+cd ../csv
+rm -rf usda
+mv ../tmp/usda .
 
-
+#
 # Clean up
-rm SR-Leg_DB.zip
-rm BFPD_db_07132018.zip
-rm cnf-fcen-csv.zip
+rm -rf ../tmp
 
-rm -rf access2csv
-rm BFPD_07132018.accdb
-rm SR_Legacy.accdb
 
-rm cnf/*.pdf
-rm "cnf/REFUSE NAME.csv"
-rm "cnf/YIELD NAME.csv"
-rm "cnf/REFUSE AMOUNT.csv"
-rm "cnf/YIELD AMOUNT.csv"
-rm "cnf/FOOD SOURCE.csv"
-rm "cnf/NUTRIENT SOURCE.csv"
-rm bfdb/Derivation_Code_Description.csv
 rm usda/DATSRCLN.csv
 rm usda/LANGUAL.csv
 rm usda/DATA_SRC.csv
@@ -74,3 +57,5 @@ rm usda/FOOTNOTE.csv
 rm usda/LANGDESC.csv
 rm usda/DERIV_CD.csv
 rm usda/SRC_CD.csv
+
+
