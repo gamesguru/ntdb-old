6440e973c89728664153111de58ba673c15954d0
gamesguru <mathmuncher11@gmail.com>
2020-01-24 14:35:59 -0500

    update internal-table.sh

--- data/internal-tables.sh
+++ data/internal-tables.sh
@@ -3,30 +3,23 @@
 cd "$(dirname "$0")"
 cd tmp
 
+#
 # Branded foods DB
 cd bfdb
-csvsql  --snifflimit 1000 --dialect postgresql Nutrient.csv > ../bfdb-Nutrient.sql
-csvsql  --snifflimit 1000 --dialect postgresql Products.csv > ../bfdb-Products.sql
-csvsql  --snifflimit 1000 --dialect postgresql Serving_Size.csv > ../bfdb-Serving_Size.sql
+( csvsql --snifflimit 100 --dialect postgresql Nutrient.csv;  csvsql --snifflimit 100 --dialect postgresql Products.csv;  csvsql --snifflimit 100 --dialect postgresql Serving_Size.csv; ) > ../bfdb.sql
 
+#
 # Canadian nutrient files
 cd ../cnf
-csvsql  --snifflimit 1000 --dialect postgresql 'NUTRIENT AMOUNT.csv' > '../cnf-NUTRIENT AMOUNT.sql'
-csvsql  --snifflimit 1000 --dialect postgresql 'FOOD NAME.csv' > '../cnf-FOOD NAME.sql'
-csvsql  --snifflimit 1000 --dialect postgresql 'CONVERSION FACTOR.csv' > '../cnf-CONVERSION FACTOR.sql'
-csvsql  --snifflimit 1000 --dialect postgresql 'MEASURE NAME.csv' > '../cnf-MEASURE NAME.sql'
-csvsql  --snifflimit 1000 --dialect postgresql 'NUTRIENT NAME.csv' > '../cnf-NUTRIENT NAME.sql'
-csvsql  --snifflimit 1000 --dialect postgresql 'FOOD GROUPS.csv' > '../cnf-FOOD GROUP.sql'
+( csvsql --snifflimit 100 --dialect postgresql 'NUTRIENT AMOUNT.csv';  csvsql --snifflimit 100 --dialect postgresql -e iso8859 'FOOD NAME.csv';  csvsql --snifflimit 100 --dialect postgresql 'CONVERSION FACTOR.csv';  csvsql --snifflimit 100 --dialect postgresql -e iso8859 'MEASURE NAME.csv';  csvsql --snifflimit 100 --dialect postgresql -e iso8859 'NUTRIENT NAME.csv';  csvsql --snifflimit 100 --dialect postgresql -e iso8859 'FOOD GROUP.csv'; ) > ../cnf.sql
 
+#
 # Open foods DB
 cd ../ofdb
-head -10000 en.openfoodfacts.org.products.csv >> en.openfoodfacts.org.products-trunc.csv
-csvsql  --snifflimit 1000 --dialect postgresql en.openfoodfacts.org.products-trunc.csv > ../ofdb.sql
+head -1000 en.openfoodfacts.org.products.csv >> en.openfoodfacts.org.products-trunc.csv
+csvsql --snifflimit 100 --dialect postgresql en.openfoodfacts.org.products-trunc.csv > ../ofdb.sql
 
+#
 # USDA standard references
 cd ../usda
-csvsql  --snifflimit 1000 --dialect postgresql FD_GROUP.csv > ../usda-FD_GROUP.sql
-csvsql  --snifflimit 1000 --dialect postgresql FOOD_DES.csv > ../usda-FOOD_DES.sql
-csvsql  --snifflimit 1000 --dialect postgresql NUT_DATA.csv > ../usda-NUT_DATA.sql
-csvsql  --snifflimit 1000 --dialect postgresql NUTR_DEF.csv > ../usda-NUTR_DEF.sql
-csvsql  --snifflimit 1000 --dialect postgresql WEIGHT.csv > ../usda-WEIGHT.sql
+( csvsql --snifflimit 100 --dialect postgresql FD_GROUP.csv;  csvsql --snifflimit 100 --dialect postgresql FOOD_DES.csv;  csvsql --snifflimit 100 --dialect postgresql NUT_DATA.csv;  csvsql --snifflimit 100 --dialect postgresql NUTR_DEF.csv;  csvsql --snifflimit 100 --dialect postgresql WEIGHT.csv; ) > ../usda.sql
