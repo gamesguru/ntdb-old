54ec90da94f5f65a1515a0cbf53b5f4cb0043b55
gamesguru <mathmuncher11@gmail.com>
2020-01-25 10:35:26 -0500

    add export.sh

---
+++ sql/export.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+DB=nutra
+SCHEMA=nt
+
+cd "$(dirname "$0")"
+cd ../data/csv/nt
+
+# Export each table
+psql -Atc "select tablename from pg_tables where schemaname='$SCHEMA'" postgresql://$LOGNAME@localhost:5432/$DB |\
+  while read TBL; do
+    psql -c "COPY $SCHEMA.$TBL TO stdout WITH csv HEADER" postgresql://$LOGNAME@localhost:5432/$DB > $TBL.csv
+  done
