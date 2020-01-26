801e3afe725cc724af943ff8c9ecd7406a1778d0
gamesguru <mathmuncher11@gmail.com>
2020-01-26 03:08:37 -0500

    set search_path in helio.sh

--- sql/helio.sh
+++ sql/helio.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+DB=nutra_dev
+SCHEMA=nt
+
 cd "$(dirname "$0")"
 source .env
 
+# SET search_path
+psql -c "ALTER ROLE $PSQL_HELIO_USER SET search_path TO $SCHEMA" postgresql://$PSQL_HELIO_USER:$PSQL_HELIO_PASSWORD@$PSQL_HELIO_HOST:5432/nutra_dev
+
 # Connect to DB
-psql postgresql://$PSQL_HELIO_USER:$PSQL_HELIO_PASSWORD@$PSQL_HELIO_HOST:5432/$PSQL_HELIO_DB_NAME
+psql postgresql://$PSQL_HELIO_USER:$PSQL_HELIO_PASSWORD@$PSQL_HELIO_HOST:5432/nutra_dev
