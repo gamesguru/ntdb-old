06f1b1ab0b8fc380f77ffa83cff0aa570e5f7f1a
gamesguru <mathmuncher11@gmail.com>
2020-01-26 06:40:14 -0500

    vscode settings, format sql

--- sql/functions.sql
+++ sql/functions.sql
@@ -8,20 +8,20 @@ SET search_path TO nt;
 -- #1
 -- Get product reviews (with username)
 --
-CREATE 
+CREATE
 OR REPLACE FUNCTION get_product_reviews(product_id VARCHAR) RETURNS TABLE(
-  username VARCHAR, rating SMALLINT, 
+  username VARCHAR, rating SMALLINT,
   review_text VARCHAR, created_at INT
-) AS $$ 
-SELECT 
-  u.username AS username, 
-  rv.rating, 
-  rv.review_text, 
-  rv.created_at 
-FROM 
-  reviews AS rv 
-  INNER JOIN users AS u ON rv.user_id = u.id 
-WHERE 
+) AS $$
+SELECT
+  u.username AS username,
+  rv.rating,
+  rv.review_text,
+  rv.created_at
+FROM
+  reviews AS rv
+  INNER JOIN users AS u ON rv.user_id = u.id
+WHERE
   rv.stripe_product_id = product_id $$ LANGUAGE SQL;
 
 --
@@ -30,4 +30,3 @@ WHERE
 -- #2
 -- Nothing
 --
-
