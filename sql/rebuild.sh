f3a975054e7f8db3b126aa695a78aed9d43fc4c7
gamesguru <mathmuncher11@gmail.com>
2020-01-27 20:30:56 -0500

    add .env, other envs, small fix rebuild.sh

--- sql/rebuild.sh
+++ sql/rebuild.sh
@@ -13,8 +13,8 @@ psql -c "\i tables.sql" postgresql://$LOGNAME@localhost:5432/$DB
 bash import.sh
 
 # Set tsvector
-psql -c "ALTER TABLE food_des ADD TextSearch_Desc tsvector NULL" postgresql://$LOGNAME@localhost:5432/$DB
-psql -c "UPDATE food_des set TextSearch_Desc = to_tsvector(long_desc)" postgresql://$LOGNAME@localhost:5432/$DB
+psql -c "ALTER TABLE $SCHEMA.food_des ADD TextSearch_Desc tsvector NULL" postgresql://$LOGNAME@localhost:5432/$DB
+psql -c "UPDATE $SCHEMA.food_des set TextSearch_Desc = to_tsvector(long_desc)" postgresql://$LOGNAME@localhost:5432/$DB
 
 # Stored Procedures
 psql -c "\i functions.sql" postgresql://$LOGNAME@localhost:5432/$DB
