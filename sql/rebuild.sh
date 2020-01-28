a2d90b52200c439133e4496dde0757dcf288627d
gamesguru <mathmuncher11@gmail.com>
2020-01-28 15:53:48 -0500

    don't use tsvector

--- sql/rebuild.sh
+++ sql/rebuild.sh
@@ -7,12 +7,8 @@ source .env
 # Create tables
 psql -c "\i tables.sql" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
 
-# Import data
-bash import.sh
-
-# Set tsvector
-psql -c "ALTER TABLE $PSQL_SCHEMA_NAME.food_des ADD TextSearch_Desc tsvector NULL" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
-psql -c "UPDATE $PSQL_SCHEMA_NAME.food_des set TextSearch_Desc = to_tsvector(long_desc)" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
-
 # Stored Procedures
 psql -c "\i functions.sql" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
+
+# Import data
+bash import.sh
